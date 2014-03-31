467c43a6b226e15472b8b7a09da52029bd28bef1
Posashkov Dmitry <k0kak0la1488@gmail.com>
2014-03-31 16:00:21 +0400

    fix the code and work with github, but did not bug.

---
+++ SplashScreen/src/com/example/splashscreen/HomeScreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.splashscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class HomeScreenActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+		addItemsToList();
+	}
+
+	/**
+	 * Add items to ListView.
+	 */
+	private void addItemsToList() {
+
+		int ITEMS_COUNT = 20;
+		ListView listView = (ListView) findViewById(R.id.listView);
+		String[] items = new String[ITEMS_COUNT];
+
+		for (int i = 0; i < ITEMS_COUNT; i++) {
+			items[i] = "item " + (i + 1);
+		}
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.my_list_item, items);
+
+		listView.setAdapter(adapter);
+	}
+}
